f0c799eaa981d2cdd13cdb2f65e636a6ba9571e8
albertgoncalves <goncalves.albert@gmail.com>
2022-12-13 18:11:14 -0500

    second part of `Day 8`

--- src/Day08.hs
+++ src/Day08.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TupleSections #-}
 {-# LANGUAGE TypeApplications #-}
 
 import Data.Array (Array, bounds, listArray, (!))
@@ -9,30 +10,61 @@ intoArray xs = listArray ((0, 0), (x, y)) $ concat $ transpose xs
     x = length (head xs) - 1
     y = length xs - 1
 
-visible :: Array (Int, Int) Int -> [(Int, Int)] -> [(Int, Int)]
-visible array path =
-  map fst $
-    nubBy (\a b -> snd a == snd b) $ f 0 $ zip path $ map (array !) path
-  where
-    f :: Int -> [(a, Int)] -> [(a, Int)]
-    f _ [] = []
-    f n (x@(_, m) : xs)
-      | n <= m = x : f m xs
-      | otherwise = f n xs
-
 part1 :: Array (Int, Int) Int -> Int
-part1 array = length $ nub $ sort $ concatMap (visible array) paths
+part1 array =
+  length $
+    nub $
+      sort $
+        concatMap visible $ concat [p0, p1, map reverse p0, map reverse p1]
   where
     (x, y) = snd $ bounds array
-    l0 = [[(j, i) | j <- [0 .. x]] | i <- [0 .. y]]
-    l1 = [[(j, i) | i <- [0 .. y]] | j <- [0 .. x]]
-    paths = concat [l0, l1, map reverse l0, map reverse l1]
+    p0 = [[(j, i) | j <- [0 .. x]] | i <- [0 .. y]]
+    p1 = [[(j, i) | i <- [0 .. y]] | j <- [0 .. x]]
+
+    visible :: [(Int, Int)] -> [(Int, Int)]
+    visible = map fst . nubBy (\a b -> snd a == snd b) . f 0
+      where
+        f :: Int -> [(Int, Int)] -> [((Int, Int), Int)]
+        f _ [] = []
+        f n (x' : xs)
+          | n <= m = (x', m) : f m xs
+          | otherwise = f n xs
+          where
+            m = array ! x'
+
+part2 :: Array (Int, Int) Int -> Int
+part2 array = maximum $ map (uncurry f) starts
+  where
+    (x, y) = snd $ bounds array
+    starts = [(j, i) | j <- [0 .. x], i <- [0 .. y]]
+
+    f :: Int -> Int -> Int
+    f j i =
+      product $
+        map
+          (visible (array ! (j, i)) . tail)
+          [ map (j,) $ reverse [0 .. i],
+            map (,i) $ reverse [0 .. j],
+            reverse $ map (j,) $ reverse [i .. y],
+            reverse $ map (,i) $ reverse [j .. x]
+          ]
+
+    visible :: Int -> [(Int, Int)] -> Int
+    visible k = length . f' k
+      where
+        f' :: Int -> [(Int, Int)] -> [(Int, Int)]
+        f' _ [] = []
+        f' n (x' : xs)
+          | array ! x' < n = x' : f' n xs
+          | otherwise = [x']
 
 main :: IO ()
 main =
   interact $
-    show
-      . part1
+    unlines
+      . map show
+      . zipWith ($) [part1, part2]
+      . replicate 2
       . intoArray
       . map (map (read @Int . (: [])))
       . lines
