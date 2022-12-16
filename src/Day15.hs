1daf1ab6e0a77b919d24da012acfc4fe84f6c98c
albertgoncalves <goncalves.albert@gmail.com>
2022-12-16 17:53:51 -0500

    second part of `Day 15`

--- src/Day15.hs
+++ src/Day15.hs
@@ -40,21 +40,27 @@ line = do
 parse :: ReadP (Int, [Pair])
 parse = (,) <$> (int <* skipSpaces) <*> (many1 (line <* skipSpaces) <* eof)
 
+distance :: Pos -> Pos -> Int
+distance (x0, y0) (x1, y1) = abs (x1 - x0) + abs (y1 - y0)
+
 rows :: Int -> Pos -> Pos -> [Row]
-rows n (x0, y0) (x1, y1) =
-  [ ([(x0 - (d - abs k), x0 + (d - abs k))], y0 + k)
+rows n sensor@(x0, y0) beacon =
+  [ ([(x0 - s, x0 + s)], y1)
     | k <- [-d .. d],
-      (y0 + k) == n
+      let y1 = y0 + k,
+      y1 == n,
+      let s = d - abs k
   ]
   where
-    d = abs (x1 - x0) + abs (y1 - y0)
+    d = distance sensor beacon
 
 merge :: Span -> [Span] -> [Span]
 merge a [] = [a]
 merge a@(l0, r0) (b@(l1, r1) : bs)
   | l1 < l0 = merge b $ a : bs
   | (l0 <= r1) && (l1 <= r0) = merge (l0, max r0 r1) bs
-  | otherwise = a : b : bs
+  | succ r0 == l1 = merge (l0, r1) bs
+  | otherwise = a : merge b bs
 
 part1 :: Int -> [Pair] -> Int
 part1 n =
@@ -64,10 +70,37 @@ part1 n =
     . flip (M.!) n
     . M.fromListWith (++)
     . map swap
-    . concatMap (uncurry (rows n))
+    . concatMap (uncurry $ rows n)
+
+pointRadius :: Pos -> Pos -> (Pos, Int)
+pointRadius sensor beacon = (sensor, distance sensor beacon)
+
+neighbors :: Pos -> Int -> [Pos]
+neighbors (x, y) radius =
+  concat
+    [ [(succ $ x + j, y + i) | j <- rs, let i = radius - j],
+      [(x + j, succ $ y + i) | i <- rs, let j = radius - i],
+      [(x - succ j, y + i) | j <- rs, let i = radius - j],
+      [(x + j, y - succ i) | i <- rs, let j = radius - i]
+    ]
+  where
+    rs = [0 .. radius]
+
+withinRange :: Pos -> Pos -> Int -> Bool
+withinRange candidate center = (distance center candidate <=)
+
+withinBounds :: Int -> Pos -> Bool
+withinBounds n (x, y) = (0 <= x) && (0 <= y) && (x <= n) && (y <= n)
 
 part2 :: Int -> [Pair] -> Int
-part2 = undefined
+part2 n pairs =
+  (\(x, y) -> (x * 4000000) + y) $
+    head $
+      filter
+        (\candidate -> not $ any (uncurry (withinRange candidate)) sensors)
+        $ filter (withinBounds $ n * 2) $ concatMap (uncurry neighbors) sensors
+  where
+    sensors = map (uncurry pointRadius) pairs
 
 main :: IO ()
 main =
