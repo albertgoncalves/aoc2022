456f24c40428200b8598dbf4f2c67a2ff9219e1c
albertgoncalves <goncalves.albert@gmail.com>
2022-12-16 18:07:07 -0500

    second part of `Day 14`

--- src/Day14.hs
+++ src/Day14.hs
@@ -15,6 +15,8 @@ import Prelude hiding (floor)
 
 type Pair = (Int, Int)
 
+type Sim = (Int -> Int -> Int -> S.Set (Int, Int) -> Maybe (S.Set (Int, Int)))
+
 int :: ReadP Int
 int = read <$> munch1 isDigit
 
@@ -30,17 +32,9 @@ expand ((x0, y0) : pairs@((x1, y1) : _))
   | otherwise = undefined
 expand _ = []
 
-sim :: Int -> Int -> Int -> S.Set (Int, Int) -> Maybe (S.Set (Int, Int))
-sim floor x0 y0 world
-  | floor < y0 = Nothing
-  | S.notMember (x0, y1) world = sim floor x0 y1 world
-  | S.notMember (x1, y1) world = sim floor x1 y1 world
-  | S.notMember (x2, y1) world = sim floor x2 y1 world
-  | otherwise = Just $ S.insert (x0, y0) world
-  where
-    y1 = succ y0
-    x1 = pred x0
-    x2 = succ x0
+sim :: Sim -> S.Set (Int, Int) -> Int
+sim f world =
+  length $ unfold (f (maximum $ map snd $ S.toList world) 500 0) world
 
 unfold :: (a -> Maybe a) -> a -> [a]
 unfold f x0 =
@@ -48,16 +42,37 @@ unfold f x0 =
     Nothing -> []
     Just x1 -> x1 : unfold f x1
 
-part1 :: S.Set (Int, Int) -> Int
-part1 world =
-  length $ unfold (sim (maximum $ map snd $ S.toList world) 500 0) world
+part1 :: Sim
+part1 floor x0 y0 world
+  | floor < y0 = Nothing
+  | S.notMember (x0, y1) world = part1 floor x0 y1 world
+  | S.notMember (x1, y1) world = part1 floor x1 y1 world
+  | S.notMember (x2, y1) world = part1 floor x2 y1 world
+  | otherwise = Just $ S.insert (x0, y0) world
+  where
+    y1 = succ y0
+    x1 = pred x0
+    x2 = succ x0
+
+part2 :: Sim
+part2 floor x0 y0 world
+  | S.member (x0, y0) world = Nothing
+  | succ floor == y0 = Just $ S.insert (x0, y0) world
+  | S.notMember (x0, y1) world = part2 floor x0 y1 world
+  | S.notMember (x1, y1) world = part2 floor x1 y1 world
+  | S.notMember (x2, y1) world = part2 floor x2 y1 world
+  | otherwise = Just $ S.insert (x0, y0) world
+  where
+    y1 = succ y0
+    x1 = pred x0
+    x2 = succ x0
 
 main :: IO ()
 main =
   interact $
     unlines
       . map show
-      . zipWith ($) [part1]
+      . zipWith sim [part1, part2]
       . repeat
       . S.fromList
       . concatMap expand
